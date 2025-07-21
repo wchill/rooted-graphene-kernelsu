8a76177903d55ffae8436a50e51a0338958b980e
Remous-Aris Koutsiamanis <aris@ariskou.com>
2025-07-21 12:10:13 +0200

    Create dirs

--- scripts/prepare-for-building-in-tmp.sh
+++ scripts/prepare-for-building-in-tmp.sh
@@ -6,6 +6,10 @@ mkdir -p /tmp/podman-run-containers
 mkdir -p /run/containers
 mount --bind /tmp/podman-run-containers /run/containers
 
+mkdir -p /tmp/aosp_mirror
+mkdir -p /tmp/adevtool_cache
+
+
 mkdir -p /dev/shm/graphene-keys
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp "$SCRIPT_DIR/passwords.sh" /dev/shm/graphene-keys
