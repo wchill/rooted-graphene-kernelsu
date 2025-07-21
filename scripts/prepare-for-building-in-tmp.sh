dd74b115f086667c0306f46ed3fafb2507967cc0
Remous-Aris Koutsiamanis <aris@ariskou.com>
2025-07-21 11:10:53 +0200

    Correct the preparation

--- scripts/prepare-for-building-in-tmp.sh
+++ scripts/prepare-for-building-in-tmp.sh
@@ -1,12 +1,15 @@
-
 mkdir -p /tmp/podman-var-lib-containers
-mount --bind podman-var-lib-containers /var/lib/containers/
+mkdir -p /var/lib/containers/
+mount --bind /tmp/podman-var-lib-containers /var/lib/containers/
+
 mkdir -p /tmp/podman-run-containers
+mkdir -p /run/containers
 mount --bind /tmp/podman-run-containers /run/containers
+
 mkdir -p /dev/shm/graphene-keys
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp "$SCRIPT_DIR/passwords.sh" /dev/shm/graphene-keys
 
 apt update -y
-apt install podman
+apt install -y podman tree
 
